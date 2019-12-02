e5c84257b7b9de257de8681c6022aa8fab0a41d3
Gili Sinai <gilisinai2016@gmail.com>
2019-12-02 11:57:19 +0200

    add main files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      transactions: [
+        { amount: 3200, vendor: "Elevation", category: "Salary" },
+        { amount: -7, vendor: "Runescape", category: "Entertainment" },
+        { amount: -20, vendor: "Subway", category: "Food" },
+        { amount: -98, vendor: "La Baguetterie", category: "Food" }
+      ]
+    }
+  }
+
+  
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+
+  }
 }
 
 export default App;
